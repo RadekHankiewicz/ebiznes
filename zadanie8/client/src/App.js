0ca1e64d694bd4cd80c099f785d35cbf69d6e71d
RadekHankiewicz <radek.hankiewicz@gmail.com>
2023-06-05 21:58:31 +0200

    zadanie 8

---
+++ zadanie8/client/src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Wysłanie danych logowania do serwera
+    const response = await fetch('http://localhost:5000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      // Logowanie udane
+      setError('');
+      console.log(data.message);
+    } else {
+      // Logowanie nieudane
+      setError(data.error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Logowanie</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Nazwa użytkownika:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Hasło:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Zaloguj się</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
+
+export default Login;
